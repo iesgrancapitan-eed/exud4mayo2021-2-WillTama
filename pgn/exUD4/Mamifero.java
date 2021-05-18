3b86d24823756be5dd4cf8ba8d0002b086b4d990
WillTama <hetagui95@gmail.com>
2021-05-18 13:34:21 +0200

    refactor:Mamifero - Guillermo Tamajon

---
+++ pgn/exUD4/Mamifero.java
@@ -0,0 +1,27 @@
+
+package pgn.exUD4;
+
+public class Mamifero {
+
+  private int edad;
+  public String nombre;
+
+  public Mamifero() {
+    super();
+  }
+
+  public void setAge(int edad) throws EdadErroneaException {
+    if (edad < 0)
+        throw new EdadErroneaException("La edad no puede ser negativa");
+    this.edad = edad;
+  }
+
+  public int getEdad() {
+    return this.edad;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+}
